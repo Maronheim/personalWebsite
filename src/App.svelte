3488d6a563d15cec8cd38476c8e5b35f324956bb
Maronheim <maar5011@colorado.edu>
2024-12-15 01:37:06 -0700

    proper image addition

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,9 @@
 <script>
   import Navigation from './lib/components/Navigation.svelte';
+  import Hero from './lib/pages/hero.svelte';
 </script>
 
 <main>
   <Navigation />
-  <h1>Welcome to My Personal Website</h1>
+  <Hero />
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 2rem;
-  }
-</style>
---
+++ src/lib/pages/hero.svelte
@@ -0,0 +1,13 @@
+<div
+  class="hero min-h-screen"
+  style="background-image: url('/images/hero.JPG'); background-size: cover; background-position: center;">
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <!-- <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.
+      </p> -->
+      <!-- <button class="btn btn-primary">Get Started</button> -->
+    </div>
+  </div>
+</div>
